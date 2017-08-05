ed7db45a561bb8545cdf8e52ad875733f28a0c47
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 06:55:16 +0900

    delete function added

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import (
     TweetCreateView,
     TweetDetailView,
+    TweetDeleteView,
     TweetListView,
     TweetUpdateView
     )
@@ -10,5 +11,6 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update')
+    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
+    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete')
 ]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+        DetailView,
+        ListView,
+        CreateView,
+        UpdateView,
+        DeleteView)
 from .models import Tweet
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .forms import TweetModelForm
@@ -29,6 +35,11 @@ class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     form_class = TweetModelForm
     template_name = 'tweets/update_view.html'
     success_url = "/tweet/"
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy("home")
 
 
 class TweetDetailView(DetailView):
