a20f0b4680a51b9df28e61f67250e57b717d4167
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 15:32:14 +0900

    timestamp表示

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -2,16 +2,13 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
     TweetListAPIView,
-    # TweetCreateView,
-    # TweetDetailView,
-    # TweetDeleteView,
-    # TweetListView,
-    # TweetUpdateView
+    TweetCreateAPIView,
     )
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
+    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
