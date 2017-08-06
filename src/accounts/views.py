3f771125c570916ecf3e2ab46d1604ee0b59153b
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 17:35:20 +0900

    profile

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.views.generic import DetailView
 from django.shortcuts import render
 
 # Create your views here.
+
+User = get_user_model()
+
+class UserDetailView(DetailView):
+    template_name = 'accounts/user_detail.html'
+    queryset = User.objects.all()
