7130c5c3b331c32868718c913da2cd3ce1e54a66
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 20:02:47 +0900

    follow/unfollowボタン切り替わり

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
     UserDetailView,
+    UserFollowView,
     )
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
     # url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete')
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
-from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+
+from .models import UserProfile
 
 # Create your views here.
 
@@ -16,3 +20,18 @@ class UserDetailView(DetailView):
             User,
             username__iexact=self.kwargs.get("username")
             )
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailView, self).get_context_data(*args, **kwargs)
+        following = UserProfile.objects.is_following(
+            self.request.user,
+            self.get_object())
+        context['following'] = following
+        return context
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated():
+            is_following = UserProfile.objects.toggle_follow(request.user, toggle_user)
+        return redirect("profiles:detail", username=username)
