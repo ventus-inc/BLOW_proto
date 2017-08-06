858a790db5abfb212d21896979cccb5ff934a159
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 19:15:30 +0900

    フォロー数表示

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,6 +3,18 @@ from django.conf import settings
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+    use_for_related_fields =True
+    def all(self):
+        qs = self.get_queryset().all()
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        print(self)
+        return qs
+
 class UserProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -12,5 +24,11 @@ class UserProfile(models.Model):
         blank=True,
         related_name='followed_by')
 
+    objects = UserProfileManager()
+
     def __str__(self):
         return str(self.following.all().count())
+
+    def get_following(self):
+        users = self.following.all()
+        return users.exclude(username=self.user.username)
