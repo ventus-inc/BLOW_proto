f41c5eac4e2ca62fcb84c2365857ae3ffb661dcd
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 17:52:52 +0900

    User profile表示

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,3 +9,10 @@ User = get_user_model()
 class UserDetailView(DetailView):
     template_name = 'accounts/user_detail.html'
     queryset = User.objects.all()
+    slug_field = 'username'
+
+    def get_object(self):
+        return get_object_or_404(
+            User,
+            username__iexact=self.kwargs.get("username")
+            )
