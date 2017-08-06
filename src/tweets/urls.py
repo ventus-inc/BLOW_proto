c4f75251567955be6914fadfdb10d9cdfcba5935
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 10:05:05 +0900

    投稿フォームを追加

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from .views import (
     TweetCreateView,
     TweetDetailView,
@@ -8,7 +9,8 @@ from .views import (
     )
 
 urlpatterns = [
-    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^search$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
