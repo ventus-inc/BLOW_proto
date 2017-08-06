b39881258eb439b460b44a8027cf6ade8a484b78
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-06 12:27:28 +0900

    searchをjqueryで動かす

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,10 +1,17 @@
 from rest_framework import generics
-
+from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+                )
+        return qs
