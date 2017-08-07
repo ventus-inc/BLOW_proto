83f2e6ba709eaa303d1dd56ffebec5694bbbf2d3
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-07 22:05:33 +0900

    single detail view

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -5,12 +5,14 @@ from .views import (
     RetweetAPIView,
     TweetListAPIView,
     TweetCreateAPIView,
+    TweetDetailAPIView,
     )
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/
+    url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'), # /api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/retweet$', RetweetAPIView.as_view(), name='retweet'),
     url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like'),
