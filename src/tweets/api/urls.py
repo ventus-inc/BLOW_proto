3b1d776c0aab69617871ffce8e0d3ad363ee9d77
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-07 09:48:16 +0900

    retweet complete

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
+    RetweetAPIView,
     TweetListAPIView,
     TweetCreateAPIView,
     )
@@ -10,7 +11,7 @@ urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
-    # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/retweet$', RetweetAPIView.as_view(), name='retweet'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete')
 ]
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,9 +1,26 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 from .pagination import StandardResultsPagination
+
+class RetweetAPIView(APIView):
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        tweet_qs = Tweet.objects.filter(pk=pk)
+        message = "Not allowed"
+        if tweet_qs.exists() and tweet_qs.count() == 1:
+            if request.user.is_authenticated():
+                new_tweet = Tweet.objects.retweet(request.user, tweet_qs.first())
+                if new_tweet is not None:
+                    data = TweetModelSerializer(new_tweet).data
+                    return Response(data)
+                message = "Cannot retweet more than twice in 1 day"
+        return Response({"message":message}, status=400)
 
 class TweetCreateAPIView(generics.CreateAPIView):
     serializer_class = TweetModelSerializer
