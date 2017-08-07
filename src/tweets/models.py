38a37c8d6df5027af595e847a2e92cdb3e762c22
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-07 10:10:54 +0900

    @userもリンクにしてハイライト

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,7 +1,9 @@
+import re
 from django.conf import settings
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils import timezone
 from .validators import validate_content
 
@@ -63,3 +65,18 @@ class Tweet(models.Model):
     #     if content == "abc":
     #         raise ValidationError("Cannot be ABC")
     #     return super(Tweet, self).clean(*args, **kwargs)
+
+def tweet_save_receiver(sender, instance, created, *args, **kwargs):
+    if created and not instance.parent:
+        # notify a user
+        user_regex = r'@(?P<username>[\w.@+-]+)'
+        usernames = re.findall(user_regex, instance.content)
+        # notification to user here
+
+
+        hash_regex = r'#(?P<hashtag>[\w.@+-]+)'
+        hashtags = re.findall(hash_regex, instance.content)
+        # sending hashtag signal here
+
+
+post_save.connect(tweet_save_receiver, sender=Tweet)
