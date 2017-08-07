336e8358e186fa9042374e6e5ac0e6f8fe8b4e2c
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-07 10:28:21 +0900

    signal done

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -5,6 +5,8 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils import timezone
+
+from hashtags.signals import parsed_hashtags
 from .validators import validate_content
 
 # Create your models here.
@@ -76,6 +78,7 @@ def tweet_save_receiver(sender, instance, created, *args, **kwargs):
 
         hash_regex = r'#(?P<hashtag>[\w.@+-]+)'
         hashtags = re.findall(hash_regex, instance.content)
+        parsed_hashtags.send(sender=instance.__class__, hashtag_list=hashtags)
         # sending hashtag signal here
 
 
