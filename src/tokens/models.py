b47e4f901689511938f92d1ea3e382bded8e4e8f
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-26 17:04:19 +0900

    sum num

--- src/tokens/models.py
+++ src/tokens/models.py
@@ -30,7 +30,7 @@ class BuyOrder(models.Model):
 	timestamp   = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return str(self.buyer)
+		return str(self.master)
 
 	class Meta:
 		ordering = ('price',)
--- src/tokens/views.py
+++ src/tokens/views.py
@@ -20,9 +20,38 @@ class UserTokenView(DetailView):
     	context = super(UserTokenView, self).get_context_data(**kwargs)
     	user = User.objects.get(username=self.kwargs.get("username"))
     	context['user'] = user
-    	context['buys'] = BuyOrder.objects.filter(master=user)
+    	context['buys'] = BuyOrder.objects.filter(master=user).order_by('-price')
     	print(context['user'])
     	print(context['buys'])
+    	total_buys = []
+    	previous_price = None
+    	obj = BuyOrder()
+    	for i in context['buys']:
+    		if not previous_price:
+    			obj = BuyOrder(
+					master = i.master,
+					buyer = i.buyer,
+					price = i.price,
+					lot = i.lot,
+    				)
+    		if not previous_price == i.price and previous_price:
+    			total_buys.append(obj)
+    			obj = BuyOrder(
+					master = i.master,
+					buyer = i.buyer,
+					price = i.price,
+					lot = i.lot,
+    				)
+    		else:
+    			obj.lot += i.lot
+    			# total_buys.append(obj)
+    		print(i.buyer)
+    		print(i.price)
+    		print(i.lot)
+    		print(len(context['buys']))
+    		previous_price = i.price
+    	total_buys.append(obj)
+    	context['total_buys'] = total_buys
     	return context
 
     def get_object(self):
@@ -35,9 +64,11 @@ class UserTokenView(DetailView):
 class BuyTokenView(LoginRequiredMixin, View):
 	def post(self, request, *args, **kwargs):
 		if request.method == 'POST' and request.user.is_authenticated():
+			master = User.objects.get(username=self.kwargs.get("username"))
 			lot = request.POST.get("lot")
 			price = request.POST.get("value")
 			obj = BuyOrder(
+				master = master,
 				buyer = request.user,
 				price = price,
 				lot = lot,
