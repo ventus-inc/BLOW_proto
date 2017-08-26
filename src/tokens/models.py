d0c6b17b4188eccec020b864ab681f145c613a72
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-08-26 19:09:42 +0900

    BuyOrderManagerに分離

--- src/tokens/models.py
+++ src/tokens/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class TokenBoard(models.Model):
 	"""売り買い板
 	"""
-	master 		= models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+	master 		= models.ForeignKey(settings.AUTH_USER_MODEL, default=None)
 	price_now 	= models.FloatField(null=True, blank=True, default=None)
 	timestamp   = models.DateTimeField(auto_now_add=True)
 
@@ -14,10 +14,39 @@ class Token(models.Model):
 	"""持っているtoken
 	"""
 	token_board 	= models.ForeignKey(TokenBoard)
-	owner			= models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+	owner			= models.ForeignKey(settings.AUTH_USER_MODEL, default=None)
 	latest_price 	= models.FloatField(null=True, blank=True, default=None)
 	updated			= models.DateTimeField(auto_now=True)
 	timestamp		= models.DateTimeField(auto_now_add=True)
+
+class BuyOrderManager(models.Manager):
+	def get_summed_lot(self, master):
+		buys = self.get_queryset().filter(master=master).order_by('-price')
+		total_buys = []
+		previous_price = None
+		obj = BuyOrder()
+		for i in buys:
+			if not previous_price:
+				obj = BuyOrder(
+					master = i.master,
+					buyer = i.buyer,
+					price = i.price,
+					lot = i.lot,
+					)
+			else:
+				if not previous_price == i.price:
+					total_buys.append(obj)
+					obj = BuyOrder(
+						master = i.master,
+						buyer = i.buyer,
+						price = i.price,
+						lot = i.lot,
+						)
+				else:
+					obj.lot += i.lot
+			previous_price = i.price
+		total_buys.append(obj)
+		return total_buys
 
 class BuyOrder(models.Model):
 	"""注文
@@ -28,6 +57,8 @@ class BuyOrder(models.Model):
 	token_board = models.ForeignKey(TokenBoard, blank=True, null=True)
 	lot			= models.IntegerField(default=0)
 	timestamp   = models.DateTimeField(auto_now_add=True)
+
+	objects = BuyOrderManager()
 
 	def __str__(self):
 		message = 'order_by:'+ str(self.master) + '\n at:'+ str(self.timestamp)
--- src/tokens/views.py
+++ src/tokens/views.py
@@ -20,8 +20,7 @@ class UserTokenView(DetailView):
     	context = super(UserTokenView, self).get_context_data(**kwargs)
     	user = User.objects.get(username=self.kwargs.get("username"))
     	context['user'] = user
-    	context['buys'] = BuyOrder.objects.filter(master=user).order_by('-price')
-    	context['total_buys'] = self._sum_lot(context['buys'])
+    	context['total_buys'] = BuyOrder.objects.get_summed_lot(user)
     	return context
 
     def get_object(self):
@@ -30,34 +29,6 @@ class UserTokenView(DetailView):
             User,
             username__iexact=self.kwargs.get("username")
         )
-
-    def _sum_lot(self, buys):
-    	total_buys = []
-    	previous_price = None
-    	obj = BuyOrder()
-    	for i in buys:
-    		if not previous_price:
-    			obj = BuyOrder(
-					master = i.master,
-					buyer = i.buyer,
-					price = i.price,
-					lot = i.lot,
-    				)
-    		else:
-	    		if not previous_price == i.price:
-	    			total_buys.append(obj)
-	    			obj = BuyOrder(
-						master = i.master,
-						buyer = i.buyer,
-						price = i.price,
-						lot = i.lot,
-	    				)
-	    		else:
-	    			obj.lot += i.lot
-    		previous_price = i.price
-    	total_buys.append(obj)
-    	return total_buys
-
 
 class BuyTokenView(LoginRequiredMixin, View):
 	def post(self, request, *args, **kwargs):
@@ -72,10 +43,6 @@ class BuyTokenView(LoginRequiredMixin, View):
 				lot = lot,
 				)
 			obj.save()
-			print(request.user)
-			print(lot)
-			print(price)
-			# return render(request, "home.html")
 			return redirect("home")
 
 	def get_object(self):
