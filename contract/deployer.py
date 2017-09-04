4acb08b0546ea2b6c28c9076e4706dcb2a5f6dd2
im tan <lacrimalelacrimal@gmail.com>
2017-09-04 14:23:14 +0900

    add contract deployer

---
+++ contract/deployer.py
@@ -0,0 +1,18 @@
+from web3 import Web3, HTTPProvider, KeepAliveRPCProvider
+import sys
+import json
+args = sys.argv
+
+web3 = Web3(KeepAliveRPCProvider(host = 'localhost', port = '8545'))
+
+token_binary = args[1]+'.bin'
+token_abi = args[1]+'.abi'
+binary = open(token_binary)
+abi = open(token_abi)
+abi = json.loads(abi.read())
+cnt = web3.eth.contract()
+cnt.bytecode = '0x'+binary.read()
+cnt.abi = abi
+web3.personal.unlockAccount(web3.eth.coinbase, 'hoge')
+cnt.deploy(transaction={'from': web3.eth.coinbase, 'gas': 1000000}, args=(100, 'My', 2, 'test'))
+
