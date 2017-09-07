151125462f5ec79384f4a233a519049bb40009ee
im tan <lacrimalelacrimal@gmail.com>
2017-09-07 14:10:18 +0900

    send_token_transactionクラスを作成

--- src/transactions/views.py
+++ src/transactions/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.views import View
 from django.shortcuts import render
 from accounts.models import WalletProfile
+import json,sys
 
 User = get_user_model()
 class SendTransactionView(View):
@@ -30,14 +31,17 @@ class SendTransactionView(View):
 class SendTokenTransactionView(View):
     def post(self,request, *args, **kwargs):
         if request.method == 'POST':
-            
-            query = request.POST.get("value")
-            value = int(query) * 100000
-            # 1Blowあたり10000weiとして送金します
-            print(value)
             from_wallet = WalletProfile.objects.get(user=request.user)
             to_user = request.POST.get("username")
             to_user = User.objects.get(username=to_user)
             to_wallet = WalletProfile.objects.get(user=to_user)
             web3 = Web3(KeepAliveRPCProvider(host='localhost', port='8545'))
+            web3.personal.unlockAccount(to_wallet.num, to_user.username)
+            # 暫定的にABIを直接入力(どのトークンでも共通)
+            abi=json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}]')
+            cnt = web3.eth.contract(abi, "0xd32a2d87f45671afdd26be4862c8c3da91ea7b43", "My")
+
+            print(cnt.abi)
+            cnt.transact(transaction={'from': web3.eth.coinbase}).transfer(to_wallet.num, 10)
+            #web3.eth.contract()
         return render(request, "home.html")
